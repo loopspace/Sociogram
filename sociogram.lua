7c964b96f038e9869c8f4f1c212e50800c66af70
Andrew Stacey <stacey@math.ntnu.no>
2014-12-31 23:41:43 +0000

    Some error checking.  TeX graph for number of negatives and positives for each student.

--- sociogram.lua
+++ sociogram.lua
@@ -3,6 +3,7 @@
 NEGATIVE = 1
 POSITIVE = 2
 ALL = 3
+GRAPH = 4
 
 function init()
    local file, c, s, o, title, ofile
@@ -39,6 +40,9 @@ function init()
 	 if v == '-a' then
 	    o = ALL
 	 end
+	 if v == '-g' then
+	    o = GRAPH
+	 end
 	 k = k + 1
 	 v = arg[k]
       end
@@ -52,7 +56,11 @@ function init()
       s = process(file)
    end
    if o then
-      doGraph(o,ofile,s,title)
+      if o == GRAPH then
+	 doTeXGraph(ofile,s,title)
+      else
+	 doGraph(o,ofile,s,title)
+      end
    end
 end
 
@@ -63,15 +71,17 @@ function doLine(line)
    i = line:find(";")
    t.name = line:sub(1,i-1)
    j = line:find(";",i+1)
-   p = line:sub(i+1,j-1)
-   if p ~= '' then
-      local k,l = 1
-      while p:find(",",k) do
-	 l = p:find(",",k)
-	 table.insert(t.positives,p:sub(k,l-1))
-	 k = l+1
+   if j > i+1 then
+      p = line:sub(i+1,j-1)
+      if p ~= '' then
+	 local k,l = 1
+	 while p:find(",",k) do
+	    l = p:find(",",k)
+	    table.insert(t.positives,p:sub(k,l-1))
+	    k = l+1
+	 end
+	 table.insert(t.positives,p:sub(k))
       end
-      table.insert(t.positives,p:sub(k))
    end
    i = j
    p = line:sub(i+1)
@@ -94,7 +104,9 @@ function process(file)
       io.input(file)
       line = io.read("*line")
       while line do
-	 table.insert(students,doLine(line))
+	 if line ~= '' then
+	    table.insert(students,doLine(line))
+	 end
 	 line = io.read("*line")
       end
    end
@@ -188,7 +200,7 @@ strict digraph Class {
   {
     node [style=filled];
 
-]])
+]]) -- " Emacs highlighting gets confused
    local nodes,edges,names,ns = {},{},{},0
    
    for k,v in ipairs(students) do
@@ -206,14 +218,22 @@ strict digraph Class {
    for k,v in ipairs(students) do
       p[k] = {}
       for l,u in ipairs(v.positives) do
-	 p[k][names[u]] = true
+	 if not names[u] then
+	    io.stderr:write ("Unknown student: ", u)
+	 else
+	    p[k][names[u]] = true
+	 end
       end
    end
    local n = {}
    for k,v in ipairs(students) do
       n[k] = {}
       for l,u in ipairs(v.negatives) do
-	 n[k][names[u]] = true
+	 if not names[u] then
+	    print ("Unknown student: ", u)
+	 else
+	    n[k][names[u]] = true
+	 end
       end
    end
 
@@ -268,6 +288,104 @@ strict digraph Class {
    end
    io.write([[
 }
+]])
+end
+
+function doTeXGraph(ofile,students,title)
+   if ofile then
+      io.output(ofile)
+   end
+   io.write([[
+\documentclass{article}
+\thispagestyle{empty}
+\usepackage{tikz}
+\usetikzlibrary{arrows.meta,positioning}
+\colorlet{pin}{black}
+\begin{document}
+\begin{tikzpicture}[
+   position/.style={
+      fill,
+      circle,
+      inner sep=2pt,
+  },
+  >=Latex,
+  every pin/.style={
+    pin position=45,
+  },
+  junior/.style={
+    text=red
+  },
+]
+]])
+   
+   local names,ns,np,nn = {},0,0,0
+   
+   for k,v in ipairs(students) do
+      names[v.name] = k
+      ns = ns + 1
+   end
+   
+   local n,p = {},{}
+   for k=1,ns do
+      p[k] = {}
+      n[k] = {}
+   end
+   for k,v in ipairs(students) do
+      for l,u in ipairs(v.positives) do
+	 if not names[u] then
+	    io.stderr:write ("Unknown student: ", u)
+	 else
+	    p[names[u]][k] = true
+	 end
+      end
+   end
+   local m
+   for k,v in pairs(p) do
+      m = 0
+      for l,u in pairs(v) do
+	 m = m + 1
+      end
+      p[k] = m
+      np = math.max(np,m)
+   end
+   for k,v in ipairs(students) do
+      for l,u in ipairs(v.negatives) do
+	 if not names[u] then
+	    print ("Unknown student: ", u)
+	 else
+	    n[names[u]][k] = true
+	 end
+      end
+   end
+   for k,v in pairs(n) do
+      m = 0
+      for l,u in pairs(v) do
+	 m = m + 1
+      end
+      n[k] = m
+      nn = math.max(nn,m)
+   end
+   io.write('\\draw[->] (-1,0) -- (',np+1,',0) node[below] {positive};',"\n")
+   io.write('\\draw[->] (0,-1) -- (0,',nn+1,') node[left] {negative};',"\n")
+   io.write('\\foreach \\k in {0,...,',np,'} { \\draw (\\k,0) +(0,2pt) -- +(0,-7pt) node[below=.15] {\\(\\k\\)}; }',"\n")
+   io.write('\\foreach \\k in {0,...,',nn,'} { \\draw (0,\\k) +(2pt,0) -- +(-7pt,0) node[left=.15] {\\(\\k\\)}; }',"\n")
+   local labels,label = {}
+   for k=1,ns do
+      label = p[k] .. ',' .. n[k]
+      if not labels[label] then
+	 labels[label] = {}
+      end
+      table.insert(labels[label], 'S' .. k)
+   end
+   for k,v in pairs(labels) do
+      io.write('\\node[position,pin={',table.concat(v,","),'}] at (',k,') {};',"\n")
+   end
+   for k=1,ns do
+      io.write('\\node[right] at (',np+1.5,',',k/2,') {S',k,' = ',students[k].name,'};',"\n");
+   end
+   io.write([[
+\end{tikzpicture}
+\end{document}
 ]])
 end
 
