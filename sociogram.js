3f52383cfd5db58827bf30bae71ccfafe3ba1e08
Andrew Stacey <stacey@math.ntnu.no>
2015-01-29 22:01:40 +0000

    Added file upload and text input code.

--- sociogram.js
+++ sociogram.js
@@ -16,26 +16,188 @@ btn.onclick = function(e) {
     return false;
 }
 
-btn = document.querySelector('#generate');
-btn.onclick = function(e) {
-    var positive = document.querySelector('#positive').checked;
-    var negative = document.querySelector('#negative').checked;
-    console.log(positive,negative);
-    var title = document.querySelector('#title').value;
+radio = document.getElementsByName('input');
+
+var formInput = document.querySelector('#formInput');
+var textInput = document.querySelector('#textInput');
+var fileInput = document.querySelector('#fileInput');
+function setInput (e) {
+    setInputAux(e.target.value);
+}
+
+function setInputAux (v) {
+    if (v == 'form') {
+	textInput.style.display = 'none';
+	fileInput.style.display = 'none';
+	formInput.style.display = 'block';
+    } else if (v == 'text') {
+	textInput.style.display = 'block';
+	fileInput.style.display = 'none';
+	formInput.style.display = 'none';
+    } else if (v == 'file') {
+	textInput.style.display = 'none';
+	fileInput.style.display = 'block';
+	formInput.style.display = 'none';
+    }
+}
+
+var chck = true;
+for (var i=0; i<radio.length; i++) {
+    radio[i].onchange = setInput;
+    if (radio[i].checked) {
+	chck = false;
+	setInputAux(radio[i].value);
+    }
+}
+if (chck) {
+    radio[1].checked = true;
+    setInputAux(radio[1].value);
+}
+
+function sociogramFromForm() {
     var rows = tbdy.getElementsByTagName('tr');
     var n = rows.length;
     var sociogram = [];
-    var cell;
+    var i;
     var re = /\s*,\s*/;
-    var i,j;
     
     for (i = 0; i<n; i++) {
 	sociogram[i] = [];
 	sociogram[i].name = rows[i].children[0].children[0].value;
 	sociogram[sociogram[i].name] = i;
-	sociogram[i].positives = rows[i].children[1].children[0].value.split(re);
-	sociogram[i].negatives = rows[i].children[2].children[0].value.split(re);
+	if (rows[i].children[1].children[0].value != '' ) {
+	    sociogram[i].positives = rows[i].children[1].children[0].value.split(re);
+	} else {
+	    sociogram[i].positives = [];
+	}
+	if (rows[i].children[2].children[0].value != '' ) {
+	    sociogram[i].negatives = rows[i].children[2].children[0].value.split(re);
+	} else {
+	    sociogram[i].negatives = [];
+	}
     }
+    return sociogram;
+}
+
+function sociogramFromText() {
+    var tbox = document.querySelector('#text');
+    return parseSociogram(tbox.value);
+}
+
+var reader = new FileReader();
+fileInput.innerHTML = fileInput.innerHTML;
+var upload = document.querySelector('#fileUpload');
+
+var fileText;
+upload.onchange = function(f) {
+    reader.onload = function(e) {
+        fileText = e.target.result;
+    }
+    reader.readAsText(f.target.files[0]);
+}
+
+function sociogramFromFile() {
+    return parseSociogram(fileText);
+}
+
+function parseSociogram(txt) {
+    var lines = txt.split("\n");
+    var sociogram = [];
+
+    lines.forEach(function(v,i) {
+	var re = /\s*;\s*/;
+	var e = v.split(re);
+	if (e.length > 1) {
+	    re = /\s*,\s*/;
+	    var entry = [];
+	    entry.name = e[0];
+	    sociogram[e[0]] = i;
+	    if (e[1] != '') {
+		entry.positives = e[1].split(re);
+	    } else {
+		entry.positives = [];
+	    }
+	    if (e[2] != '') {
+		entry.negatives = e[2].split(re);
+	    } else {
+		entry.negatives = [];
+	    }
+	    sociogram.push(entry);
+	}
+    });
+
+    return sociogram;
+}
+
+btn = document.querySelector('#generate');
+btn.onclick = function(e) {
+    setTimeout(generateSociogram,100);
+    return false;
+}
+
+function generateSociogram() {
+    
+    var positive = document.querySelector('#positive').checked;
+    var negative = document.querySelector('#negative').checked;
+    var title = document.querySelector('#title').value;
+    var cell;
+    var i,j;
+    var positives;
+    var negatives;
+    var sociogram;
+
+    var input = document.querySelector('input[name="input"]:checked').value;
+
+    if (input == "form") {
+	sociogram = sociogramFromForm();
+    } else if (input == "text") {
+	sociogram = sociogramFromText();
+    } else if (input == "file") {
+	sociogram = sociogramFromFile();
+    }
+
+    n = sociogram.length;
+    j = n;
+    
+    for (i = 0; i<n; i++) {
+	sociogram[i].positives.forEach(function(v) {
+	    if (typeof sociogram[v] === "undefined") {
+		sociogram[j] = [];
+		sociogram[j].name = v;
+		sociogram[v] = j;
+		sociogram[j].positives = [];
+		sociogram[j].negatives = [];
+		j++;
+	    }
+	});
+	sociogram[i].negatives.forEach(function(v) {
+	    if (typeof sociogram[v] === "undefined") {
+		sociogram[j] = [];
+		sociogram[j].name = v;
+		sociogram[v] = j;
+		sociogram[j].positives = [];
+		sociogram[j].negatives = [];
+		j++;
+	    }
+	});
+    }
+    n = j;
+    
+    positives = [];
+    negatives = [];
+    for (i = 0; i < n; i++) {
+	positives[i] = [];
+	negatives[i] = [];
+    }
+
+    sociogram.forEach(function(v,i) {
+	v.positives.forEach(function(u) {
+	    positives[i][sociogram[u]] = true;
+	});
+	v.negatives.forEach(function(u) {
+	    negatives[i][sociogram[u]] = true;
+	});
+    });
 
     var dot = 'strict digraph Class { splines=true; overlap=orthoyx; label="' + title + '"; labeloc=b; labeljust=center; fontsize=30; { node [style=filled]; ';
 
@@ -46,33 +208,51 @@ btn.onclick = function(e) {
     dot += '} ';
 
     if (positive) {
-	dot += '{ edge [color="red"]; ';
-//	dot += '{ edge [dir="both"]; ';
+	dot += '{ edge [color="blue"]; ';
+	dot += '{ edge [dir="both"]; ';
 	for (i=0; i<n; i++) {
-	    sociogram[i].positives.forEach(function(v) {
-		if (sociogram[v]) {
-		    dot += ' STUDENT' + i + ' -> STUDENT' + sociogram[v] + '; ';
+	    for (j=i; j<n; j++) {
+		if (positives[i][j] && positives[j][i]) {
+		    dot += ' STUDENT' + i + ' -> STUDENT' + j + '; ';
 		}
-	    })
+	    }
+	}
+	dot += '} ';
+	for (i=0; i<n; i++) {
+	    for (j=0; j<n; j++) {
+		if (positives[i][j] && !positives[j][i]) {
+		    dot += ' STUDENT' + i + ' -> STUDENT' + j + '; ';
+		}
+	    }
 	}
 	dot += '} ';
     }
     
     if (negative) {
-	dot += '{ edge [color="blue"]; ';
-//	dot += '{ edge [dir="both"]; ';
+	dot += '{ edge [color="red"]; ';
+	dot += '{ edge [dir="both"]; ';
 	for (i=0; i<n; i++) {
-	    sociogram[i].negatives.forEach(function(v) {
-		if (sociogram[v]) {
-		    dot += ' STUDENT' + i + ' -> STUDENT' + sociogram[v] + '; ';
+	    for (j=i; j<n; j++) {
+		if (negatives[i][j] && negatives[j][i]) {
+		    dot += ' STUDENT' + i + ' -> STUDENT' + j + '; ';
 		}
-	    })
+	    }
+	}
+	dot += '} ';
+	for (i=0; i<n; i++) {
+	    for (j=0; j<n; j++) {
+		if (negatives[i][j] && !negatives[j][i]) {
+		    dot += ' STUDENT' + i + ' -> STUDENT' + j + '; ';
+		}
+	    }
 	}
 	dot += '} ';
     }
 
     dot += '} ';
     
-    console.log(dot);
+    svg = Viz(dot, "svg");
+    var out = document.querySelector("#output");
+    out.innerHTML = svg;
     return false;
 }
