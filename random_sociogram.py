239c3b529b083ffeea6927824ee4d47b76206477
Andrew Stacey <loopspace@mathforge.org>
2023-06-27 22:01:32 +0100

    Random generator
    The random sociogram generator now produces longer "names" and ensures
    that no-one selects themselves.

--- random_sociogram.py
+++ random_sociogram.py
@@ -1,10 +1,13 @@
 import random
 
-letters = [chr(x + 97) for x in range(26)]
+letters = [chr(x + 65) + chr(x+97)*3 for x in range(26)]
 
 for i in range(26):
-     print(chr(i+97), end="; ")
-     random.shuffle(letters)
-     n = random.randint(0,4)
-     print(", ".join(letters[0:n]), end="; ")
-     print(", ".join(letters[n:n+random.randint(0,4)]))
+     nletters = letters[:i] + letters[i+1:]
+     random.shuffle(nletters)
+     m = random.randint(1,4)
+     n = random.randint(0,3)
+     if m+n > 0:
+          print(letters[i], end="; ")
+          print(", ".join(nletters[0:m]), end="; ")
+          print(", ".join(nletters[m:m+n]))
